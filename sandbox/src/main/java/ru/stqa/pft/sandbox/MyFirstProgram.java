0b193a92340fd7dc382cf2569377c796be7b0c97
irene_r <irina.reta@kreditech.com>
2016-07-21 00:13:19 +0300

    task 1-3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,27 @@
 package ru.stqa.pft.sandbox;
 
+
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
 		System.out.println("Hello,World!");
-	}
+
+    Point p1 = new Point();
+    Point p2 = new Point();
+
+    p1.x = 2.0;
+    p2.x = 1.0;
+    p1.y = 3.0;
+    p2.y = 7.1;
+
+    System.out.println("Point 1 has coodrinate:" + p1.x + " " + p1.y);
+    System.out.println("Point 2 has coodrinate:" + p2.x + " " + p2.y);
+    System.out.println("Distance is:" + distance(p1,p2));
+  }
+
+
+  public static double distance(Point p1, Point p2){
+    return  Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
+  }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by irener on 7/20/16.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+
+
+}
