6e9877ccb8102b2c40dc8ded249635da992fad7c
irene_r <irina.reta@kreditech.com>
2016-07-21 17:19:16 +0300

    update for lesson1.task #4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,22 +6,31 @@ public class MyFirstProgram {
 	public static void main (String[] args) {
 		System.out.println("Hello,World!");
 
-    Point p1 = new Point();
-    Point p2 = new Point();
+    Point p1 = new Point(2.0, 1.0);
+    Point p2 = new Point(3.0, 7.1);
 
     p1.x = 2.0;
     p2.x = 1.0;
     p1.y = 3.0;
     p2.y = 7.1;
 
-    System.out.println("Point 1 has coodrinate:" + p1.x + " " + p1.y);
-    System.out.println("Point 2 has coodrinate:" + p2.x + " " + p2.y);
+    System.out.println("Point 1 has coodrinate: " + p1.x + " " + p1.y);
+    System.out.println("Point 2 has coodrinate: " + p2.x + " " + p2.y);
     System.out.println("Distance is:" + distance(p1,p2));
+
+
+    //lesson1.task #4
+
+    System.out.println("Point 1 has coodrinate: " + p1.x + " " + p1.y);
+    System.out.println("Point 2 has coodrinate: " + p2.x + " " + p2.y);
+    System.out.println("Distance is: " + p1.distance2(p2));
+    //System.out.println("Distance is:" + p2.distance2(p1));
   }
 
 
   public static double distance(Point p1, Point p2){
     return  Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
   }
+
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,4 +1,6 @@
 package ru.stqa.pft.sandbox;
+import java.lang.Math;
+
 
 /**
  * Created by irener on 7/20/16.
@@ -6,8 +8,15 @@ package ru.stqa.pft.sandbox;
 public class Point {
 
   public double x;
-  public double y;
+  public double y;nf
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
 
-
+  public double distance2(Point other){
+  return Math.sqrt((this.x - x)*(this.x - x)+(this.y - y)*(this.y - y));
+}
 }
