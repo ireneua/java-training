8da067255da51638d41c8571afabe3bedff33f87
irene_r <irina.reta@kreditech.com>
2016-07-23 21:27:12 +0300

    updated tests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 /**
  * Created by irener on 7/21/16.
  */
@@ -14,14 +15,33 @@ public class PointTest {
     Point c = new Point(3.1, 0.0);
     Point b = new Point(1.2, 3.0);
 
+    Assert.assertEquals(c.distance2(b), 3.55, 2);
+
+  }
+
+  @Test
+  public void testDistance2(){
+
+    Point c = new Point(3.1, 0.0);
+    Point b = new Point(1.2, 3.0);
+
     //distance is positive value
     Assert.assertEquals(c.distance2(b) >= 0, true);
 
     //distance between c-b is equal to distance betweeb b-c
     Assert.assertEquals(c.distance2(b), b.distance2(c));
 
-    //distance is calculated correctly
-    Assert.assertEquals(c.distance2(b), 3.55, 2);
+  }
+
+  @Test
+  public void testDistance3(){
+
+    Point c = new Point(3.1, 0.0);
+    Point b = new Point(1.2, 3.0);
+
+    //distance between c-b is equal to distance between b-c
+    Assert.assertEquals(c.distance2(b), b.distance2(c));
 
   }
+
 }
