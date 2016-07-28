91f14140e4d59956c2b2e1f8bfd8a8613e93d25c
irene_r <irina.reta@kreditech.com>
2016-07-28 16:23:37 +0300

    updated GroupDeletion

--- ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-<<<<<<< HEAD
 import org.testng.annotations.Test;
 
 
@@ -15,31 +14,5 @@ public class GroupDeletionTests extends TestBase{
     returnToGroupPage();
   }
 
-=======
-import org.openqa.selenium.By;
-import org.testng.annotations.Test;
-
-public class GroupDeletionTests extends TestBase {
-
-  @Test
-  public void testGroupDeletionTests() {
-
-    goToGroupPage();
-    selectGroup();
-    deleteGroup();
-    returnToGroupPage();
-  }
-
-  private void deleteGroup() {
-    wd.findElement(By.name("delete")).click();
-  }
-
-  private void selectGroup() {
-    if (!wd.findElement(By.name("selected[]")).isSelected()) {
-      wd.findElement(By.name("selected[]")).click();
-    }
-  }
-
->>>>>>> origin/master
 
 }
