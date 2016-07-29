b03085135c19788bc466dd2e579dea830bc36577
irene_r <irina.reta@kreditech.com>
2016-07-29 23:09:31 +0300

    groupmodificationtest

---
+++ ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by irener on 7/29/16.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+
+  public void testGroupModification(){
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("modification", "header_modi", "footer_modi"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
