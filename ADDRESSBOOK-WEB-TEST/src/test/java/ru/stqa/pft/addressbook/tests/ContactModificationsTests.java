224d965a69355b3826d552ed099ef178fe31ee10
irene_r <irina.reta@kreditech.com>
2016-08-08 17:17:50 +0300

    task 3.9

--- ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationsTests.java
+++ ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationsTests.java
@@ -11,6 +11,9 @@ public class ContactModificationsTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().goToHomePage();
+    if (!app.getContactHelper().isThereAContact()){
+      app.getContactHelper().createContact(new ContactData("Johny", "Dow", "Po", null, "developer", "Po-dev", null, "indo@test.com8", "indo_double@test.com", "indo8_double@test.com", "1978"));
+    }
     app.getContactHelper().editContact();
     app.getContactHelper().fillContactData(new ContactData("Judi", "Jaster", "Ko", "pro", "developer", "Po-dev", "USA", "indo6@test.com", "indo_doublex@test.com", "indo_doublex@test.com", "1967"));
     app.getContactHelper().saveUpdatedContact();
--- ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -12,6 +12,9 @@ public class GroupModificationTests extends TestBase {
 
   public void testGroupModification() {
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test123", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("modification", "header_modi", "footer_modi"));
