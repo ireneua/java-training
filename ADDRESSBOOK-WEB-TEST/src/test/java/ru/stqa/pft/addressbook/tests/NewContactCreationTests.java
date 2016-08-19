fc731a9ac25a170f27e9d10967b1f6eed516f22c
irene_r <irina.reta@kreditech.com>
2016-08-19 14:29:25 +0300

    updated ContactDeletion

--- ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
+++ ADDRESSBOOK-WEB-TEST/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
@@ -15,19 +15,15 @@ public class NewContactCreationTests extends TestBase {
     app.goTo().homePage();
     Set<ContactData> before = app.contact().allConacts();
     app.contact().initContactCreation();
-    ContactData contact = (new ContactData()
+    /*ContactData contact = (new ContactData()
             .withName("Judi").withLastName("Jaster").withCompany("Yahoo").withTitle("QA").withEmail("indo6@test.com").withEmail2("indo_doublex@test.com")
-            .withYear("1987"));
+            .withYear("1987"));*/
     app.contact().fillContactData(contact);
     app.contact().returnToHomePage();
     Set<ContactData> after = app.contact().allConacts();
     contact.withId((after.stream().mapToInt((g) -> g.getId()).max().getAsInt()));
 
     before.add(contact);
-    //contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-    /*Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
-    before.sort(byId);
-    after.sort(byId);*/
     Assert.assertEquals(before, after);
 
   }
